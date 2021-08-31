2e3d2ca150108e93f35ece4bf5fbc7fc3fa01a7f
Ix5231 <ix5231@gmail.com>
2021-08-31 20:50:26 +0900

    add test for other chord

--- src/theory/__test__/chord.test.ts
+++ src/theory/__test__/chord.test.ts
@@ -1,7 +1,7 @@
 import { findChord, ChordSelection } from 'theory/chord';
 
 describe('findChord', () => {
-  test('C#メジャーが見つけられる', () => {
+  test('C#が見つけられる', () => {
     const sel: ChordSelection = {
       root: 1,
       others: [5, 8],
@@ -11,7 +11,8 @@ describe('findChord', () => {
       quality: 'major',
     }]);
   });
-  test('Bメジャーが見つけられる', () => {
+
+  test('Bが見つけられる', () => {
     const sel: ChordSelection = {
       root: 11,
       others: [3, 6],
@@ -19,6 +20,39 @@ describe('findChord', () => {
     expect(findChord(sel)).toEqual([{
       root: 11,
       quality: 'major',
+    }]);
+  });
+
+  test('Dmが見つけられる', () => {
+    const sel: ChordSelection = {
+      root: 2,
+      others: [5, 9],
+    };
+    expect(findChord(sel)).toEqual([{
+      root: 2,
+      quality: 'minor',
+    }]);
+  });
+
+  test('Adimが見つけられる', () => {
+    const sel: ChordSelection = {
+      root: 9,
+      others: [0, 3],
+    };
+    expect(findChord(sel)).toEqual([{
+      root: 9,
+      quality: 'diminished',
+    }]);
+  });
+
+  test('Eaugが見つけられる', () => {
+    const sel: ChordSelection = {
+      root: 4,
+      others: [8, 0],
+    };
+    expect(findChord(sel)).toEqual([{
+      root: 4,
+      quality: 'augumented',
     }]);
   });
 });
