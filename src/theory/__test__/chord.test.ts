d16a81f1d9c15cb81f83a860c5181505b06b7c1c
Ix5231 <ix5231@gmail.com>
2021-09-13 21:14:54 +0900

    WIP

--- src/theory/__test__/chord.test.ts
+++ src/theory/__test__/chord.test.ts
@@ -1,171 +1,38 @@
 import {
-  findChord, ChordSelection, Chord, chordPrettyPrint,
+  findChord, ChordSelection, Chord
 } from 'theory/chord';
 
 describe('findChord', () => {
-  test('C#が見つけられる', () => {
-    const sel: ChordSelection = {
-      type: 'withRoot',
-      root: 1,
-      others: [5, 8],
-    };
-    expect(findChord(sel)).toEqual([{
-      root: 1,
-      base: 1,
-      quality: 'major',
-      tensions: [],
-    }]);
-  });
-
-  test('Bが見つけられる', () => {
-    const sel: ChordSelection = {
-      type: 'withRoot',
-      root: 11,
-      others: [3, 6],
-    };
-    expect(findChord(sel)).toEqual([{
-      root: 11,
-      base: 11,
-      quality: 'major',
-      tensions: [],
-    }]);
-  });
-
-  test('C#が見つけられる（ルート指定なし）', () => {
-    const sel: ChordSelection = {
-      type: 'withoutRoot',
-      pitches: [1, 5, 8],
-    };
-    expect(findChord(sel)).toEqual([{
-      root: 1,
-      base: 1,
-      quality: 'major',
-      tensions: [],
-    }]);
-  });
-
-  test('C#/B#、C#Maj7/B#、E#m(11)/B#が見つけられる', () => {
-    const sel: ChordSelection = {
-      type: 'withRoot',
-      root: 0,
-      others: [1, 5, 8],
-    };
-    const result = findChord(sel);
-    expect(result).toEqual(
-      expect.arrayContaining(
-        [
-          {
-            root: 1,
-            base: 0,
-            quality: 'major',
-            tensions: [],
-          },
-          {
-            root: 1,
-            base: 0,
-            quality: 'major',
-            tensions: [11],
-          },
-          {
-            root: 5,
-            base: 0,
-            quality: 'minor',
-            tensions: [8],
-          },
-        ],
-      ),
-    );
-  });
-
-  test('C(omit3)が見つけられる', () => {
-    const sel: ChordSelection = {
-      type: 'withRoot',
-      root: 0,
-      others: [7],
-    };
-    const result = findChord(sel);
-    expect(result).toEqual(
-      expect.arrayContaining(
-        [
-          {
-            root: 0,
-            base: 0,
-            quality: 'omit3',
-            tensions: [],
-          },
-        ],
-      ),
-    );
-  });
-});
-
-describe('chordPrettyPrint', () => {
-  const testData: [ string, Chord][] = [
-    [
-      'E', {
-        root: 4, base: 4, quality: 'major', tensions: [],
+  const withRoot: {select: ChordSelection, find: Chord}[] = [
+    {
+      // E
+      select: {
+        type: 'withRoot',
+        base: 4,
+        others: [8, 11]
       },
-    ],
-    [
-      'D♯', {
-        root: 3, base: 3, quality: 'major', tensions: [],
-      },
-    ],
-    [
-      'Cm', {
-        root: 0, base: 0, quality: 'minor', tensions: [],
-      },
-    ],
-    [
-      'Gm', {
-        root: 7, base: 7, quality: 'minor', tensions: [],
-      },
-    ],
-  ];
-
-  test.each(testData)('コード名生成: %s', (name, chord) => {
-    expect(chordPrettyPrint(chord)).toEqual(name);
-  });
-});
-
-const expects = [
-  [{
-    root: 0,
-    intervals: [4, 7, 11, 6],
-  }, {
-    root: 0,
-    base: 0,
-    baseQuality: 'major',
-    tension: [6, 11],
-  }, {
-    root: 0,
-    base: 0,
-    baseQuality: 'major7',
-    tension: [6],
-  }, {
-    name: 'CMaj7(+11)',
-    intervals: ['R', 'M3', 'P5', 'M7', '+11'],
-  }],
-];
-
-const data = [
-  {
-    id: 'major',
-    intervals: ['R', 'M3', 'P5'],
-    variant: {
-      major7: {
-        intervals: ['M7'],
+      find: {
+        base: 4,
+        id: 'major',
       },
     },
-  },
-  {
-    id: 'major7',
-    intervals: ['R', 'M3', 'P5', 'M7'],
-  },
-  {
-    id: 'major9',
-    intervals: ['R', 'M3', 'P5', 'M7', '9'],
-  },
-];
+    {
+      // Fm
+      select: {
+        type: 'withRoot',
+        base: 5,
+        others: [8, 0]
+      },
+      find: {
+        base: 5,
+        id: 'major',
+      },
+    }
+  ]
+
+  test.each(withRoot)('Chord recognize (w/root) test #%#', ({select, find}) => {
+    expect(findChord(select)).toEqual(expect.arrayContaining([find]))
+  })
+})
 
 export { };
--- src/theory/chord.ts
+++ src/theory/chord.ts
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import chords from './chords.json'
 
 export type Pitch = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11;
 export type Interval = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11;
@@ -16,115 +17,21 @@ interface ChordIntervals {
 }
 
 export interface Chord {
-  root: Pitch,
   base: Pitch,
-  quality: ChordQuality
-  tensions: Interval[],
+  id: string,
 }
 
-const chordQualities: ChordIntervals[] = [
-  {
-    name: 'major',
-    intervals: [4, 7],
-  },
-  {
-    name: 'minor',
-    intervals: [3, 7],
-  },
-  {
-    name: 'diminished',
-    intervals: [3, 6],
-  },
-  {
-    name: 'augumented',
-    intervals: [4, 8],
-  },
-  {
-    name: 'omit3',
-    intervals: [7],
-    forbidden: [3, 4],
-  },
-];
-
-export interface ChordSelectionWithRoot {
+export interface ChordSelectionWithBase {
   type: 'withRoot',
-  root: Pitch,
+  base: Pitch,
   others: Pitch[],
 }
 
-export interface ChordSelectionWithoutRoot {
+export interface ChordSelectionWithoutBase {
   type: 'withoutRoot',
   pitches: Pitch[],
 }
 
-export type ChordSelection = ChordSelectionWithRoot | ChordSelectionWithoutRoot;
+export type ChordSelection = ChordSelectionWithBase | ChordSelectionWithoutBase;
 
-const matchChord = (intervals: Interval[]) => (ci: ChordIntervals): boolean => (
-  ci.intervals.every((e) => intervals.includes(e))
-  && !ci.forbidden?.some((e) => intervals.includes(e))
-);
-
-const findChordWithRoot = (sel: ChordSelectionWithRoot): Chord[] => {
-  const intervals = sel.others.map((p) => calcInterval(sel.root, p));
-  return chordQualities
-    .filter(matchChord(intervals))
-    .map((c) => ({
-      root: sel.root,
-      base: sel.root,
-      quality: c.name,
-      tensions: intervals.filter((e) => !c.intervals.includes(e)),
-    }));
-};
-
-const findChordWithoutRoot = (sel: ChordSelectionWithoutRoot): Chord[] => sel.pitches
-  .map((e) => ({
-    type: 'withRoot',
-    root: e,
-    others: sel.pitches.filter((e2) => e2 !== e),
-  } as ChordSelectionWithRoot))
-  .flatMap(findChordWithRoot);
-
-const findChordWithBase = (sel: ChordSelectionWithRoot): Chord[] => {
-  const sel1: ChordSelectionWithoutRoot = {
-    type: 'withoutRoot',
-    pitches: [sel.root, ...sel.others],
-  };
-  const sel2: ChordSelectionWithoutRoot = {
-    type: 'withoutRoot',
-    pitches: _.cloneDeep(sel.others),
-  };
-  return [sel1, sel2].flatMap(
-    findChordWithoutRoot,
-  )
-    .map((c) => ({
-      ...c,
-      base: sel.root,
-    }));
-};
-
-export const findChord = (sel: ChordSelection): Chord[] => (sel.type === 'withRoot'
-  ? findChordWithBase(sel)
-  : findChordWithoutRoot(sel));
-
-// TODO: deal with name with flat
-const pitchName: Record<Pitch, string> = {
-  0: 'C',
-  1: 'C♯',
-  2: 'D',
-  3: 'D♯',
-  4: 'E',
-  5: 'F',
-  6: 'F',
-  7: 'G',
-  8: 'G♯',
-  9: 'A',
-  10: 'A♯',
-  11: 'B',
-};
-
-export const chordPrettyPrint = (chord: Chord): string => {
-  if (chord.quality === 'minor') {
-    return `${pitchName[chord.base]}m`;
-  }
-  return pitchName[chord.base];
-};
+export const findChord = (selection: ChordSelection) => {}
